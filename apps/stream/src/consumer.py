a02c09f375aba8d4f2da4669e07bb13e6310c2d1
2uanDM <hokage321xxx@gmail.com>
2024-12-15 17:41:42 +0700

    fix

--- apps/stream/src/consumer.py
+++ apps/stream/src/consumer.py
@@ -7,13 +7,8 @@ import geopandas as gpd
 from clickhouse_connect.driver.client import Client
 from dotenv import load_dotenv
 from kafka import KafkaConsumer
-from pydantic import BaseModel
 
 load_dotenv()
-
-
-class SimulationConfig(BaseModel):
-    speed: int = 1
 
 
 class ClickHouseResource:
@@ -22,19 +17,49 @@ class ClickHouseResource:
         self.username = username
         self.password = password
 
+    def init_table(self, client: Client):
+        query = f"""
+        CREATE TABLE IF NOT EXISTS {os.getenv("CLICKHOUSE_TABLE")}
+        (
+            segment_id       UInt32,  -- Assuming segment IDs are integers
+            speed            Float32, -- Speed is a floating-point number
+            congestion_level String,
+            from_street      String,  -- From-street name as a string
+            to_street        String,  -- To-street name as a string
+            street           String,  -- Street name as a string
+            length           Float32, -- Length as a floating-point number
+            hour             UInt8,   -- Hour as an 8-bit unsigned integer (0-23)
+            day_of_week      UInt8,
+            month            UInt8,
+            start_latitude   Float32,
+            start_longitude  Float32,
+            end_latitude     Float32,
+            end_longitude    Float32,
+        )
+        ENGINE = MergeTree()
+        ORDER BY segment_id;
+        """
+
+        client.command(query)
+
     def get_client(self) -> Client:
         prefix = self.address.split("://")[0]
         host, port = self.address.removeprefix(f"{prefix}://").split(":")
-        return clickhouse_connect.get_client(
+        client = clickhouse_connect.get_client(
             host=host,
             port=int(port),
             username=self.username,
             password=self.password,
         )
 
+        # Init the table
+        self.init_table(client)
+
+        return client
+
 
 class StreamTrafficConsumer:
-    def __init__(self, config: SimulationConfig = SimulationConfig()):
+    def __init__(self):
         self.consumer = KafkaConsumer(
             "traffic",
             bootstrap_servers=f"{os.getenv('KAFKA_HOST')}:{os.getenv('KAFKA_PORT')}",
@@ -43,7 +68,6 @@ class StreamTrafficConsumer:
             auto_offset_reset="earliest",
             group_id="traffic-consumer-group",
         )
-        self.config = config
         self.clickhouse_client = ClickHouseResource(
             address=f"http://{os.getenv('CLICKHOUSE_HOST')}:{os.getenv('CLICKHOUSE_PORT')}",
             username="",
@@ -99,6 +123,7 @@ class StreamTrafficConsumer:
 
     def run(self):
         print("Starting Kafka Consumer...")
+        print("Current kakfa topics:", list(self.consumer.topics()))
 
         for message in self.consumer:
             timestamp = message.key
@@ -119,7 +144,22 @@ class StreamTrafficConsumer:
 
                 print(f"Inserted {data} to ClickHouse")
 
+            self.consumer.commit()
+
 
 if __name__ == "__main__":
+    # Check all the environment variables are set
+    print("Checking environment variables...")
+
+    for env_var in [
+        "KAFKA_HOST",
+        "KAFKA_PORT",
+        "CLICKHOUSE_HOST",
+        "CLICKHOUSE_PORT",
+        "CLICKHOUSE_TABLE",
+    ]:
+        print(f"Checking {env_var}...")
+        print(f"In the environment: {os.getenv(env_var)}")
+
     consumer = StreamTrafficConsumer()
     consumer.run()
